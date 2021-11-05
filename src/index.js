542989a375ff906e0ef168409cd81a8f974cfaa8
Odinokun <odinokun@gmail.com>
2021-11-05 17:35:59 +0200

    added styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Person from "./person";
 
+import './styles/style.css';
+import './styles/style.scss'
+
 const Elena = new Person('Elena', 30, 'Ukraine');
 
 console.log(Elena);
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,17 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+
   entry: {
     main: "./src/index.js"
   },
+
   output: {
     clean: true,
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+
   plugins: [
     new HTMLWebpackPlugin({
       filename: "index.html",
@@ -21,5 +24,25 @@ module.exports = {
       filename: "about.html",
       template: path.resolve(__dirname, 'src/about.html')
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  }
 }
